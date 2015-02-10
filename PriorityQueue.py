186b50ff95070dbf9d23f9a2080a298a2a4bf372
jsannyjr <jsanny1@lion.lmu.edu>
2015-02-09 21:49:27 -0800

    PriorityQueue.py
    This is homework 1, Priority Queue, that its priority is the lowest
    number. Must import math before running the program.

---
+++ PriorityQueue.py
@@ -0,0 +1,81 @@
+class PriorityQueue:
+	import math
+	def __init__(self):
+		self.length = 0
+		self.listQ = []
+
+	def peek(self):
+		if self.is_empty():
+			raise ValueError( "Loser! It was empty! NOOOOO!")
+		return self.listQ[0]
+
+	def add(self, item):
+		n = 0
+		while n < self.length:
+			if self.listQ[n] == item:
+				raise ValueError("Nope, that's a duplicate.") 
+			n += 1
+		self.listQ.append(item)
+		self.length += 1
+		templength = self.length
+		if templength == 1:
+			pass
+		else:
+			while self.listQ[math.floor((templength - 1) / 2) ] > item or self.listQ[math.floor((templength - 2) / 2)] > item:
+				if templength == 1:
+					break
+				elif templength % 2 == 0 and self.listQ[math.floor((templength - 1) / 2) ] > item:
+					self.listQ[templength - 1] = self.listQ[math.floor((templength - 1) / 2)]
+					self.listQ[math.floor((templength - 1) / 2)] = item
+					templength = math.floor((templength) / 2)
+				elif templength % 2 == 1 and self.listQ[math.floor((templength - 2) / 2)] > item:
+					self.listQ[templength - 1] = self.listQ[math.floor((templength - 2) / 2)]
+					self.listQ[math.floor((templength - 2) / 2)] = item
+					templength = math.floor((templength - 1) / 2)
+				else:
+					break
+	def remove(self):
+		n = 0
+		if self.is_empty():
+			raise ValueError( "Nope, nothing's there!")
+		firstitem = self.peek()
+		self.listQ[0] = self.listQ[self.length - 1] 
+		item = self.peek()
+		self.listQ.pop()
+		self.length -= 1
+		while n < self.length - 1:
+			if (2 * n + 1) > self.length - 1:
+				break
+			elif (2 * n + 2) > self.length - 1 and 2 * n + 1 == self.length:
+				self.listQ[n] = self.listQ[2 * n + 1]
+				self.listQ[2 * n + 1] = item
+				break
+			elif (2 * n + 2) > self.length - 1:
+				self.listQ[n] = self.listQ[2 * n + 1]
+				self.listQ[2 * n + 1] = item
+				n = 2* n + 1
+			elif self.listQ[2 * n + 2] > self.listQ[2 * n + 1]:
+				self.listQ[n] = self.listQ[2 * n + 1]
+				self.listQ[2 * n + 1] = item
+				n = 2 * n + 1
+			elif self.listQ[2 * n + 2] < self.listQ[2 * n + 1]:
+				self.listQ[n] = self.listQ[2 * n + 2]
+				self.listQ[2 * n + 2] = item
+				n = 2 * n + 2
+			else: 
+				break
+		return firstitem
+	def __len__(self):
+		return str(self.length)
+
+	def is_empty(self):
+		return self.length == 0
+
+	def __str__(self):
+		n = 0
+		result = "["
+		while self.length > n:
+			result += str(self.listQ[n]) + ','
+			n += 1
+		result += "]"
+		return result
